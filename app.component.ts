9807e97b5ccf22ec7e4b215bc49140e83b711148
himangkydutta <himangkydutta08@gmail.com>
2017-07-02 20:09:32 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+//Adding component
+@Component({
+  selector: 'my-app-component',
+  templateUrl: './app.component.html'
+})
+export class RootComponent {
+  title = 'Lodding Component....';
+}
